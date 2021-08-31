e067958d0fbd3524ea05be11f27a344e196808fe
tarun2506 <tarunsharma8920@gmail.com>
2021-08-31 16:39:48 +0530

    changes

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  FlatList,
+  TextInput,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import Todo from "./component/Todo";
 export default function App() {
+  const [todos, setTodos] = useState(["task 1", "task 2"]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>Today's Todos</Text>
+        <View style={styles.list}>
+          {todos.map((todo, index) => (
+            <TouchableOpacity key={index} onPress={() => console.log("gg")}>
+              <Todo text={todo}></Todo>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <TextInput style={styles.input} placeholder="Type your todo..." />
+        <TouchableOpacity>
+          <AntDesign name="pluscircle" color="black" size={50} />
+        </TouchableOpacity>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +40,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingVertical: 60,
+    paddingHorizontal: 15,
+    justifyContent: "space-between",
+  },
+  header: {
+    justifyContent: "flex-start",
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  list: {
+    marginTop: 18,
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 10,
+    maxWidth: "80%",
+    borderColor: "#333",
+    height: 50,
+    width: "70%",
   },
 });
---
+++ component/Todo.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+function Todo(props) {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.text}>{props.text}</Text>
+      <TouchableOpacity>
+        <MaterialCommunityIcons name="trash-can" size={24} />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: "#D2D2D2",
+    marginVertical: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+    borderRadius: 10,
+  },
+  text: {
+    fontSize: 20,
+  },
+});
+
+export default Todo;
